0681f30d24d661c131ed513944f17c1f1e5104b0
Muhammad Asad Bilal Arif <asadbilalarif@gmail.com>
2022-12-22 21:47:22 +0500

    22-Dec-2022

--- DrawingTheme/Controllers/AccountController.cs
+++ DrawingTheme/Controllers/AccountController.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using DrawingTheme.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace DrawingTheme.Controllers
 {
     public class AccountController : Controller
     {
+        AutomatischeEntities DB = new AutomatischeEntities();
         // GET: Account
         public ActionResult Index()
         {
@@ -15,8 +18,79 @@ namespace DrawingTheme.Controllers
         }
         public ActionResult Login()
         {
+            FormsAuthentication.SignOut();
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(string Email, string Password)
+        {
+            string pass = null;
+            tblLog LogData = new tblLog();
+            try
+            {
+                if (Password != null)
+                {
+                    byte[] EncDataBtye = new byte[Password.Length];
+                    EncDataBtye = System.Text.Encoding.UTF8.GetBytes(Password);
+                    pass = Convert.ToBase64String(EncDataBtye);
+                }
+                var User = DB.tblUsers.Select(r => r).Where(x => x.Email == Email && x.Password == pass && x.isActive != false).FirstOrDefault();
+                if (User != null)
+                {
+
+
+                    //Session["User"] = DB.tblUsers.Select(r => r).Where(x => x.Email == Email).FirstOrDefault();
+                    Session["Access"] = DB.tblAccessLevels.Select(r => r).Where(x => x.RoleId == User.RoleId && x.isActive == true).ToList();
+                    //Session["Settings"] = DB.tblSettings.Select(r => r).FirstOrDefault();
+                    HttpCookie cookie = new HttpCookie("User");
+
+                    cookie["Email"] = User.Email;
+                    cookie["UserId"] = User.UserId.ToString();
+                    cookie["Role"] = User.tblRole.Role;
+                    cookie.Expires = DateTime.Now.AddMonths(1);
+                    Response.Cookies.Add(cookie);
+
+
+                    FormsAuthentication.SetAuthCookie(Email, false);
+
+                    User.LastLogin = DateTime.Now;
+                    DB.Entry(User);
+                    DB.SaveChanges();
+
+                    LogData.UserId = User.UserId;
+                    LogData.Action = "Login";
+                    LogData.CreatedDate = DateTime.Now;
+                    DB.tblLogs.Add(LogData);
+                    DB.SaveChanges();
+
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    var UserCheck = DB.tblUsers.Select(r => r).Where(x => x.Email == Email && x.Password == pass).FirstOrDefault();
+                    if (UserCheck != null && UserCheck.isActive == false)
+                    {
+                        ViewBag.Error = "User is in-active";
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Invalid Email or Password";
+                    }
+
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                Console.WriteLine("Error" + ex.Message);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
         public ActionResult Register()
         {
             return View();
--- DrawingTheme/Controllers/UserController.cs
+++ DrawingTheme/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DrawingTheme.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,20 +11,419 @@ namespace DrawingTheme.Controllers
 {
     public class UserController : Controller
     {
+        AutomatischeEntities DB = new AutomatischeEntities();
         // GET: User
         public ActionResult Profile()
         {
             return View();
         }
 
-        public ActionResult UserList()
+        public ActionResult UserList(string Success, string Update, string Delete, string Error)
         {
-            return View();
+            List<tblUser> UserList = DB.tblUsers.ToList();
+            ViewBag.RoleList = DB.tblRoles.ToList();
+           
+            ViewBag.Success = Success;
+            ViewBag.Update = Update;
+            ViewBag.Delete = Delete;
+            ViewBag.Error = Error;
+            return View(UserList);
         }
 
-        public ActionResult Roles()
+
+        [HttpPost]
+        public ActionResult AddUser(tblUser User, HttpPostedFileBase Image)
         {
-            return View();
+            tblUser Data = new tblUser();
+            try
+            {
+                //HttpCookie cookieObj = Request.Cookies["User"];
+                //int UserId = Int32.Parse(cookieObj["UserId"]);
+                int UserId = 1;
+                if (User.UserId == 0)
+                {
+                    if (DB.tblUsers.Select(r => r).Where(x => x.Email == User.Email).FirstOrDefault() == null)
+                    {
+                        Data = User;
+                        byte[] EncDataBtye = new byte[User.Password.Length];
+                        EncDataBtye = System.Text.Encoding.UTF8.GetBytes(User.Password);
+                        Data.Password = Convert.ToBase64String(EncDataBtye);
+                       
+
+                        string folder = Server.MapPath(string.Format("~/{0}/", "Uploading"));
+                        if (!Directory.Exists(folder))
+                        {
+                            Directory.CreateDirectory(folder);
+                        }
+                        string path = null;
+
+                        if (Image != null)
+                        {
+                            path = Path.Combine(Server.MapPath("~/Uploading"), Path.GetFileName(Image.FileName));
+                            Image.SaveAs(path);
+                            path = Path.Combine("\\Uploading", Path.GetFileName(Image.FileName));
+                            Data.ImagePath = path;
+                        }
+
+                        Data.Address = User.Address;
+                        Data.Phone = User.Phone;
+                        Data.CreatedDate = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+                        Data.CreatedBy = UserId;
+                        Data.EditDate = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+                        Data.EditBy = UserId;
+                        Data.isActive = User.isActive;
+                        DB.tblUsers.Add(Data);
+                        DB.SaveChanges();
+
+                        
+
+
+                        tblLog LogData = new tblLog();
+                        LogData.UserId = UserId;
+                        LogData.Action = "Add User";
+                        LogData.CreatedDate = DateTime.Now;
+                        DB.tblLogs.Add(LogData);
+                        DB.SaveChanges();
+
+                        return RedirectToAction("UserList", new { Success = "User has been add successfully." });
+                    }
+                    else
+                    {
+                        //ViewBag.Error = "User Already Exsist!!!";
+                        return RedirectToAction("UserList", new { Error = "User Already Exsist!!!" });
+                    }
+                }
+                else
+                {
+                    var check = DB.tblUsers.Select(r => r).Where(x => x.Email == User.Email).FirstOrDefault();
+                    if (check == null || check.UserId == User.UserId)
+                    {
+                        Data = DB.tblUsers.Select(r => r).Where(x => x.UserId == User.UserId).FirstOrDefault();
+                        //Data.username = User.username;
+
+                        string folder = Server.MapPath(string.Format("~/{0}/", "Uploading"));
+                        if (!Directory.Exists(folder))
+                        {
+                            Directory.CreateDirectory(folder);
+                        }
+                        string path = null;
+
+                        if (Image != null)
+                        {
+                            path = Path.Combine(Server.MapPath("~/Uploading"), Path.GetFileName(Image.FileName));
+                            Image.SaveAs(path);
+                            path = Path.Combine("\\Uploading", Path.GetFileName(Image.FileName));
+                            Data.ImagePath = path;
+                        }
+
+
+
+                        Data.UserId = User.UserId;
+                        Data.FirstName = User.FirstName;
+                        Data.LastName = User.LastName;
+                        Data.Email = User.Email;
+                        Data.Address = User.Address;
+                        Data.Phone = User.Phone;
+                        Data.EditDate = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+                        Data.EditBy = UserId;
+                        Data.isActive = User.isActive;
+
+                        Data.RoleId = User.RoleId;
+                        if (User.Password != null)
+                        {
+                            byte[] EncDataBtye = new byte[User.Password.Length];
+                            EncDataBtye = System.Text.Encoding.UTF8.GetBytes(User.Password);
+                            Data.Password = Convert.ToBase64String(EncDataBtye);
+                        }
+                        Data.EditDate = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+                        Data.EditBy = UserId;
+                        DB.Entry(Data);
+                        DB.SaveChanges();
+                       
+
+
+
+                        tblLog LogData = new tblLog();
+                        LogData.UserId = UserId;
+                        LogData.Action = "Update User";
+                        LogData.CreatedDate = DateTime.Now;
+                        DB.tblLogs.Add(LogData);
+                        DB.SaveChanges();
+
+                        return RedirectToAction("UserList", new { Update = "User has been Update successfully." });
+                    }
+                    else
+                    {
+                        ViewBag.Error = "User Already Exsist!!!";
+                        return RedirectToAction("UserList", new { Error = "User Already Exsist!!!" });
+                    }
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                Console.WriteLine("Error" + ex.Message);
+            }
+
+            return RedirectToAction("Users");
         }
+
+
+        [HttpPost]
+        public ActionResult DeleteUser(int UserId)
+        {
+            tblUser Data = new tblUser();
+            //HttpCookie cookieObj = Request.Cookies["User"];
+            //int CUserId = Int32.Parse(cookieObj["UserId"]);
+            int CUserId = 1;
+            try
+            {
+                Data = DB.tblUsers.Select(r => r).Where(x => x.UserId == UserId).FirstOrDefault();
+                DB.Entry(Data).State = EntityState.Deleted;
+                DB.SaveChanges();
+
+                tblLog LogData = new tblLog();
+                LogData.UserId = CUserId;
+                LogData.Action = "Delete User";
+                LogData.CreatedDate = DateTime.Now;
+                DB.tblLogs.Add(LogData);
+                DB.SaveChanges();
+
+                return RedirectToAction("UserList", new { Delete = "User has been delete successfully." });
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                Console.WriteLine("Error" + ex.Message);
+            }
+
+            return Json(0);
+        }
+
+        public ActionResult Roles(int? id, string Success, string Update, string Error)
+        {
+            List<tblRole> Roles = DB.tblRoles.Where(x => x.isActive == true).ToList();
+            ViewBag.MenuList = DB.tblMenus.ToList();
+
+            try
+            {
+                var check = DB.tblAccessLevels.Where(x => x.RoleId == id).FirstOrDefault();
+                if (id != null && id != 0 && check != null)
+                {
+                    ViewBag.SelectedRole = DB.tblRoles.Where(x => x.RoleId == id).FirstOrDefault();
+
+                    ViewBag.SelectedMenuAccess = (from h in DB.tblMenus
+                                                  join t in DB.tblAccessLevels.Where(x => x.RoleId == id) on h.MenuId equals t.MenuId into gj
+                                                  from acc in gj.DefaultIfEmpty()
+                                                      //where acc.roleid == id
+                                                  select new MenuAccess
+                                                  {
+                                                      menu = h,
+                                                      //accesslevelid = acc.accesslevelid,
+                                                      accessedit = acc.EditAccess,
+                                                      accessdelete = acc.DeleteAccess,
+                                                      accesscreate = acc.CreateAccess,
+                                                      isactive = acc.isActive,
+                                                      roleid = id,
+                                                      menuid = h.MenuId,
+
+                                                  }).ToList();
+                }
+                else
+                {
+                    if (id != null && id != 0)
+                    {
+                        ViewBag.SelectedRole = DB.tblRoles.Where(x => x.RoleId == id).FirstOrDefault();
+                    }
+                    else
+                    {
+                        ViewBag.SelectedRole = null;
+                    }
+                    ViewBag.SelectedMenuAccess = null;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                Console.WriteLine("Error" + ex.Message);
+            }
+
+
+            ViewBag.Update = Update;
+            ViewBag.Success = Success;
+            ViewBag.Error = Error;
+            return View(Roles);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult CreateAccessLevel(List<tblAccessLevel> Items, string Role, int RoleId = 0)
+        {
+            tblRole Data = new tblRole();
+            try
+            {
+                //HttpCookie cookieObj = Request.Cookies["User"];
+                //int UserId = Int32.Parse(cookieObj["UserId"]);
+                int UserId = 1;
+
+                if (RoleId != 0)
+                {
+
+                    tblRole check = DB.tblRoles.Select(r => r).Where(x => x.Role == Role).FirstOrDefault();
+                    if (check == null || check.RoleId == RoleId)
+                    {
+                        Data = DB.tblRoles.Select(r => r).Where(x => x.RoleId == RoleId).FirstOrDefault();
+
+                        Data.Role = Role;
+                        Data.isActive = true;
+                        Data.EditDate = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+                        Data.EditBy = UserId;
+                        DB.Entry(Data);
+                        DB.SaveChanges();
+                        foreach (tblAccessLevel AccessLevel in Items)
+                        {
+                            var Del = DB.tblAccessLevels.Select(r => r).Where(x => x.RoleId == AccessLevel.RoleId && x.MenuId == AccessLevel.MenuId).FirstOrDefault();
+                            if (Del != null)
+                            {
+                                DB.tblAccessLevels.Remove(Del);
+                            }
+                        }
+
+                        foreach (tblAccessLevel AccessLevel in Items)
+                        {
+                            AccessLevel.RoleId = Data.RoleId;
+                            AccessLevel.CreatedDate = DateTime.Now;
+                            AccessLevel.CreatedBy = UserId;
+                            AccessLevel.EditDate = DateTime.Now;
+                            AccessLevel.EditBy = UserId;
+                            //AccessLevel.tblMenu = DB.tblMenus.Select(r => r).Where(x => x.MenuID == AccessLevel.MenuID).FirstOrDefault();
+                            //AccessLevel.tblRole = DB.tblRoles.Select(r => r).Where(x => x.RoleID == AccessLevel.RoleID).FirstOrDefault();
+                            DB.tblAccessLevels.Add(AccessLevel);
+                            DB.SaveChanges();
+                        }
+                        DB.SaveChanges();
+
+                        tblLog LogData = new tblLog();
+                        LogData.UserId = UserId;
+                        LogData.Action = "Update Role";
+                        LogData.CreatedDate = DateTime.Now;
+                        DB.tblLogs.Add(LogData);
+                        DB.SaveChanges();
+
+                        return Json(1);
+                    }
+                    else
+                    {
+                        return Json(2);
+                    }
+
+                }
+                else
+                {
+                    if (DB.tblRoles.Select(r => r).Where(x => x.Role == Role).FirstOrDefault() == null)
+                    {
+                        Data.Role = Role;
+                        Data.isActive = true;
+                        Data.CreatedDate = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+                        Data.CreatedBy = UserId;
+                        Data.EditDate = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+                        Data.EditBy = UserId;
+                        DB.tblRoles.Add(Data);
+                        DB.SaveChanges();
+
+                        foreach (tblAccessLevel AccessLevel in Items)
+                        {
+                            var Del = DB.tblAccessLevels.Select(r => r).Where(x => x.RoleId == Data.RoleId && x.MenuId == AccessLevel.MenuId).FirstOrDefault();
+                            if (Del != null)
+                            {
+                                DB.tblAccessLevels.Remove(Del);
+                            }
+                        }
+
+                        foreach (tblAccessLevel AccessLevel in Items)
+                        {
+                            AccessLevel.RoleId = Data.RoleId;
+                            AccessLevel.CreatedDate = DateTime.Now;
+                            AccessLevel.CreatedBy = UserId;
+                            AccessLevel.EditDate = DateTime.Now;
+                            AccessLevel.EditBy = UserId;
+                            //AccessLevel.tblMenu = DB.tblMenus.Select(r => r).Where(x => x.MenuID == AccessLevel.MenuID).FirstOrDefault();
+                            //AccessLevel.tblRole = DB.tblRoles.Select(r => r).Where(x => x.RoleID == AccessLevel.RoleID).FirstOrDefault();
+                            DB.tblAccessLevels.Add(AccessLevel);
+                            DB.SaveChanges();
+                        }
+                        DB.SaveChanges();
+
+                        tblLog LogData = new tblLog();
+                        LogData.UserId = UserId;
+                        LogData.Action = "Create Role";
+                        LogData.CreatedDate = DateTime.Now;
+                        DB.tblLogs.Add(LogData);
+                        DB.SaveChanges();
+
+                        return Json(0);
+                    }
+                    else
+                    {
+                        return Json(2);
+                    }
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                Console.WriteLine("Error" + ex.Message);
+            }
+
+            return Json(0);
+        }
+
+        [HttpPost]
+        public ActionResult GetAccessLevel(int RoleId = 0)
+        {
+            List<MenuAccess> MenuAccess = new List<MenuAccess>();
+            List<tblMenu> Menu = new List<tblMenu>();
+            DB.Configuration.ProxyCreationEnabled = false;
+            try
+            {
+                Menu = DB.tblMenus.ToList();
+                MenuAccess = (from h in DB.tblMenus
+                              join t in DB.tblAccessLevels.Where(x => x.RoleId == RoleId) on h.MenuId equals t.MenuId into gj
+                              from acc in gj.DefaultIfEmpty()
+                              select new MenuAccess
+                              {
+                                  menu = h,
+                                  //accesslevelid = acc.accesslevelid,
+                                  accessedit = acc.EditAccess,
+                                  accessdelete = acc.DeleteAccess,
+                                  accesscreate = acc.CreateAccess,
+                                  isactive = acc.isActive,
+                                  roleid = RoleId,
+                                  menuid = h.MenuId,
+
+                              }).ToList();
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.ex = ex.Message;
+                Console.WriteLine("Error" + ex.Message);
+            }
+
+            return Json(MenuAccess, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
